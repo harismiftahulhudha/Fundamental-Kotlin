8b0b2c2f4fd25b4222b66445174b3a644f1ccea2
Haris Miftahul Hudha <harismiftahulhudha@gmail.com>
2020-06-29 23:17:50 +0700

    update tutorial 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,59 @@
 fun main () {
-    println("Hello World")
-    membeliBakso()
+    var panjang: Int = 10
+    var lebar: Int = 5
+    println(panjang)
+    println(lebar)
+
+    var nama: String = "Haris Miftahul Hudha"
+    println(nama)
+
+    var celcius: Float = 2.5f
+    println(celcius)
+
+    var fahrenheit: Double = 3.5
+    println(fahrenheit)
+
+    var apakahBerhasil: Boolean = false
+    println(apakahBerhasil)
+
+    var abjad: Char = 'a'
+    println(abjad)
 }
 
-fun membeliBakso() {
-    println("Membeli Bakso")
-}
+/**
+ * 1. Int (integer) digunakan untuk menampung data angka bilangan bulat
+ *    contoh: 1, 2, 3, 10, 14, dll.
+ * 2. String digunakan untuk menampung data berupa teks. penulisan sintaks tipe data String menggungakan petik 2 ("")
+ *    contoh: "belajar kotlin", "nama saya haris", dll.
+ * 3. Float digunakan untuk menampung data berupa angka bilangan pecahan. penulisan sintaks tipe data Float menggunakan huruf f di akhir angka
+ *    contoh: 1.4f, 2.4f, 3.1f, dll.
+ * 4. Double digunakan untuk menampung data berupa angka bilangan pecahan. dapat menampung dengan jumlah data yang lebih besar
+ *    dari tipe data Float. penulisan sintaks tipe data Float tanpa menggunakan huruf f di akhir angka
+ *    contoh : 1.5, 2.4, 3.7, dll.
+ * 5. Boolean digunakan untuk menampung data berupa true atau false.
+ * 6. Char digunakan untuk menampung data berupa karakter. penulisan sintaks tipe data Char menggunakan petik 1 ('')
+ *    contoh : 'a', 'b', 'c', dll.
+ *
+**/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
